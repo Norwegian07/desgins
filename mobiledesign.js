e11b1ffe95b4914da701419325e9279575eb58da
Norwegian07 <88303771+Norwegian07@users.noreply.github.com>
2022-05-11 12:42:52 +0530

    Add files via upload

---
+++ mobiledesign.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("nav-contains").style.width = "100%";
+    document.getElementById("nav-holder").style.display = "none";
+}
+function closeNav(){
+    document.getElementById("nav-contains").style.width = "0%"
+    document.getElementById("nav-holder").style.display = "block";
+
+}

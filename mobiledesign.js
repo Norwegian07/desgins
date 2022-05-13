8d437b77d6f0b568c1ae05196781f7086c239d5a
Norwegian07 <88303771+Norwegian07@users.noreply.github.com>
2022-05-13 21:29:58 +0530

    Add files via upload

--- mobiledesign.js
+++ mobiledesign.js
@@ -7,3 +7,20 @@ function closeNav(){
     document.getElementById("nav-holder").style.display = "block";
 }
 
+var initialIndex = 0;
+showSlides();
+
+function showSlides(){
+    let i;
+    initialIndex ++;
+    let slides = document.getElementsByClassName("image-holder");
+    for(i = 0; i < slides.length ; i ++){
+        slides[i].style.display = "none";
+    }
+    if(initialIndex > slides.length){
+        initialIndex = 1;
+    }
+    slides[initialIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 2000)
+}
